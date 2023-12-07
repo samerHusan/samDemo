93c8bfc23f34c24209d207fd12180138cd4673bc
samer husan <samerhusan.knit9999@gmail.com>
2023-12-07 21:10:32 +0530

    no message

--- samDemo/ViewController.swift
+++ samDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         print("Hello")
      print("I m testing")
         print("I m New")
+        print("neewww")
         // Do any additional setup after loading the view.
     }
 
