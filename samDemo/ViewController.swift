604d3a1ff8bf77cb728a883b7c4f3e1543cffc00
samer husan <samerhusan.knit9999@gmail.com>
2023-12-07 21:51:07 +0530

    Hello

--- samDemo/ViewController.swift
+++ samDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+        print("neewww")
      print("I m testing")
         print("I m New")
         print("neewww")
