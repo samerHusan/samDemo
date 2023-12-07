6c5045da7c9ee103442aa03f80cc8d4777708e23
samer husan <samerhusan.knit9999@gmail.com>
2023-12-07 15:53:30 +0530

    #newMsg

--- samDemo/ViewController.swift
+++ samDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+     print("I m testing")
         // Do any additional setup after loading the view.
     }
 
